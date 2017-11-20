15ab38d405ae95d96f2ca459b65b40269c2dd878
Jorge <j.alvarezh1994@unitec.edu>
2017-11-20 06:19:58 -0600

    Se avanzo ArbolB Jorge

--- ArbolB/src/arbolb/Main.java
+++ ArbolB/src/arbolb/Main.java
@@ -1,9 +1,11 @@
+package arbolb;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package arbolb;
+
 
 import java.util.Arrays;
 import java.util.LinkedList;
